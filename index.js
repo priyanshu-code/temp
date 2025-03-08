0d4bb1fc0b0e76beb5d27bf2dd6ed0e7743cac84
priyanshu-code <rawatpriyanshu007@gmail.com>
2025-03-08 16:07:59 +0530

    cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/config/database.js';
 import pathRoutes from './src/routes/pathRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Connect to database
 connectDB();
